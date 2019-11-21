e734e4375c5641735317ec9e22245851db12909a
Zeyi Hou <50049694+zeyihou@users.noreply.github.com>
2019-11-21 20:45:34 +0800

    Update Hungarian.cpp

--- Hungarian.cpp
+++ Hungarian.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 const int N = 2;
 
-int girl[N] = {0,0};
+int girl[N] = {100,100};   //足够大的一个初始值，标志没有分配过
 int used[N] = {0,0};
 int m=N, n=N;
 int line[N][N] = { { 1, 1 }, { 1, 0 } };
@@ -18,7 +18,7 @@ bool find(int x){
 			//如果有连线并且还没有标记过(这里标记的意思是这次查找曾试图改变过该目标的归属问题，但是没有成功，所以就不用瞎费工夫了）  
 		{
 			used[j] = 1;
-			if (girl[j] == 0 || find(girl[j])) {
+			if (girl[j] == 100 || find(girl[j])) {
 				//名花无主或者能腾出个位置来，这里使用递归  
 				girl[j] = x;
 				return true;
