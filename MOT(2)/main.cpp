2f5b62d4be1e7ef7340241409b868031530a3cf5
Zeyi Hou <50049694+zeyihou@users.noreply.github.com>
2020-01-06 19:08:41 +0800

    Update main.cpp

--- MOT(2)/main.cpp
+++ MOT(2)/main.cpp
@@ -5,6 +5,7 @@
 #include<opencv2/opencv.hpp>
 #include<cmath>
 #include<stdlib.h>
+#include<Python.h>
 
 #include "segmentation.h"
 #include "Hungarian.h"
@@ -63,6 +64,14 @@ int main()
 	association();
 
 	waitKey(0);
+
+	Py_Initialize(); /*初始化python解释器,告诉编译器要用的python编译器*/
+	PyRun_SimpleString("import matplotlib.pyplot as plt"); /*调用python文件*/
+	PyRun_SimpleString("plt.plot([1,2,3,4], [12,3,23,231])"); /*调用python文件*/
+	PyRun_SimpleString("plt.show()"); /*调用python文件*/
+	Py_Finalize(); /*结束python解释器，释放资源*/
+
+
 	return 0;
 }
 
